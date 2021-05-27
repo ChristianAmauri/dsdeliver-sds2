f3766cf63f435d2595bbc5250e8f955482ece785
Christian Amauri <christyan2@hotmail.com>
2021-05-27 14:09:16 -0300

    Update index.tsx

--- front-web/Src/rodapé/index.tsx
+++ front-web/Src/rodapé/index.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as InstagramIcon } from "./instagram.svg";
 function Footer() {
   return (
     <footer className="main-footer">
-        App desenvolvido durante a 2ª ed. do evento <b>Christian Amauri</b>
+        App desenvolvido por <b>Christian Amauri</b>
         <div className="footer-icons">
           <a href="https://www.youtube.com/channel/UCVE4lvHRe0LJG-oJsMHfcDg" target="_new">
             <YouTubeIcon />
